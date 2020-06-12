4930b952c6c30f9f11819caab61709d9b0fd61cc
Matt Rudge <mrudge@gmail.com>
2020-06-12 14:03:20 +0000

    add extra fields and fail reasons

--- js/script.js
+++ js/script.js
@@ -53,6 +53,30 @@ document.getElementById("sendForm").addEventListener("click", function () {
     req1 = document.getElementById("data1");
     req2 = document.getElementById("data2");
 
+    fail1 = document.getElementById("fail1");
+    fail2 = document.getElementById("fail2");
+    fail3 = document.getElementById("fail3");
+    fail4 = document.getElementById("fail4");
+    fail5 = document.getElementById("fail5");
+    fail5text = document.getElementById("fail5text");
+
+    failText = "";
+
+    if (fail1.checked)
+        failText += "The README.md is absent or is missing basic information\n";
+    if (fail2.checked)
+        failText +=
+            "The project plagiarises others' work without any attribution\n";
+    if (fail3.checked)
+        failText +=
+            "The deployed version is markedly different from the source code that was submitted\n";
+    if (fail4.checked)
+        failText +=
+            "The project doesn't use one of the main technologies taught in the stream.\n";
+    if (fail5.checked) failText += fail5text.value;
+
+    document.getElementById("failreasons").value = failText;
+
     if (count == completed && req1.checked && req2.checked) {
         score = document.getElementById("score").innerText;
         if (
